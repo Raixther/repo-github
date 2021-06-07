ffa7269e665a4e124012f140eb8e99bdaac6dbf8
Raixther <81520277+Raixther@users.noreply.github.com>
2021-06-07 17:59:28 +0300

    Add files via upload

---
+++ lesson_1(3)/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1_3_
+{
+    class Program
+    {
+
+        public class TestCase
+        {
+            public int X { get; set; }
+
+            public int Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        static void TestFoo(TestCase testCase)
+        {
+            try
+            {
+                var actual = CycleFibonanchi(testCase.X);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                   
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+
+        }
+
+
+
+        static int RecursiveFibonanchi(int number)
+        {
+            if (number < 2)
+            {
+                return number;
+            }
+            else
+            {
+                return RecursiveFibonanchi(number - 1) + RecursiveFibonanchi(number - 2);
+            }
+
+        }
+
+
+        static int CycleFibonanchi(int number)
+        {
+            int a = 1;
+
+            int b = 1;
+
+          
+
+            for (int i = 1; i < number; i++)
+            {
+                a += b;
+                b = a - b;
+            }
+            return b;
+        }
+
+        static void Main(string[] args)
+        {
+            TestCase test1 = new TestCase()
+            {
+                X = 1,
+
+                Expected = 1,
+
+                ExpectedException = null
+            };
+
+            TestCase test2 = new TestCase()
+            {
+                X = 3,
+
+                Expected = 2,
+
+                ExpectedException = null
+            };
+
+            TestCase test3 = new TestCase()
+            {
+                X = 8,
+
+                Expected = 21,
+
+                ExpectedException = null
+            };
+
+            TestCase test4 = new TestCase()
+            {
+                X = 9,
+
+                Expected = 34,
+
+                ExpectedException = null
+            };
+
+            TestCase test5 = new TestCase()
+            {
+                X = 6,
+
+                Expected = 8,
+
+                ExpectedException = null
+            };
+
+            TestFoo(test1);
+            TestFoo(test2);
+            TestFoo(test3);
+            TestFoo(test4);
+            TestFoo(test5);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lesson_1/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_1
+{
+    class Program
+    {
+        public class TestCase
+        {
+            public int X { get; set; }
+
+            public bool Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+
+        static bool CheckSimplicity(int number)
+        {
+            int d = 0;
+
+            int i = 2;
+
+
+            while (i < number)
+            {
+                if (number % i == 0)
+                {
+                    d++;
+                    i++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            if (d == 0)
+            {
+
+
+                return true;
+               
+              
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void TestCheckSimplicity(TestCase testCase)
+        {
+            try
+            {
+                var actual = CheckSimplicity(testCase.X);
+
+                if (actual == testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (testCase.ExpectedException != null)
+                {
+                    //TODO add type exception tests;
+                    Console.WriteLine("VALID TEST");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            var testCase1 = new TestCase()
+            {
+                X = 1,
+                Expected = true,
+                ExpectedException = null
+            };
+            var testCase2 = new TestCase()
+            {
+                X = 7,
+                Expected = true,
+                ExpectedException = null
+            };
+            var testCase3 = new TestCase()
+            {
+                X = 17,
+                Expected = true,
+                ExpectedException = null
+            };
+            var testCase4 = new TestCase()
+            {
+                X = 18,
+                Expected = false,
+                ExpectedException = null
+            };
+            var testCase5 = new TestCase()
+            {
+                X = 10,
+                Expected = false,
+                ExpectedException = null
+            };
+
+
+            TestCheckSimplicity(testCase1);
+
+            TestCheckSimplicity(testCase2);
+
+            TestCheckSimplicity(testCase3);
+
+            TestCheckSimplicity(testCase4);
+
+            TestCheckSimplicity(testCase5);
+       
+            Console.ReadKey();
+        }
+
+
+    }
+
+
+}
